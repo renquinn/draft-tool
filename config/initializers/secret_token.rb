0f7e6b6b32896dfa819a460b42103ee2e13ae3d9
Ren Quinn <renquinn@gmail.com>
2013-06-22 11:19:29 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DraftTool::Application.config.secret_token = '60685d657b7897dda03bbbd2de68d0f74f35d3bf201f0e753983684d05e1ddc88488585bb4572948438dac47c1f084c00cb6aac7fd089fd8e4d4a45a79efb3f6'

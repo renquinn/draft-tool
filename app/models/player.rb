9d03c2fb8045b7865ab93bb42ea6a79e10691a59
Ren Quinn <renquinn@gmail.com>
2013-06-29 17:06:51 -0600

    Add sorting and tabbing by position of players table

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,6 +4,12 @@ class Player < ActiveRecord::Base
 
   scope :selected, where(:selected => true)
   scope :unselected, where(:selected => false)
+  scope :qb, where(:position => "QB")
+  scope :rb, where(:position => "RB")
+  scope :wr, where(:position => "WR")
+  scope :te, where(:position => "TE")
+  scope :k, where(:position => "K")
+  scope :dst, where(:position => "D/ST")
 
   def calculate_score
     score = 0
@@ -29,5 +35,44 @@ class Player < ActiveRecord::Base
   def mark_unselected!
     self.selected = false
     save!
+  end
+
+  def self.order_by(column, players)
+    column ||= "rank"
+
+    case column
+    when "points"
+      players = players.sort { |a,b| b.calculate_score <=> a.calculate_score }
+    when "rank"
+      players = players.sort { |a,b| a.rank <=> b.rank }
+    when "name"
+      players = players.sort { |a,b| a.name <=> b.name }
+    when "nfl_team"
+      players = players.sort { |a,b| a.nfl_team <=> b.nfl_team }
+    when "completions"
+      players = players.sort { |a,b| b.completions <=> a.completions }
+    when "attempts"
+      players = players.sort { |a,b| b.attempts <=> a.attempts }
+    when "passing_yards"
+      players = players.sort { |a,b| b.passing_yards <=> a.passing_yards }
+    when "passing_touchdowns"
+      players = players.sort { |a,b| b.passing_touchdowns <=> a.passing_touchdowns }
+    when "interceptions"
+      players = players.sort { |a,b| a.interceptions <=> b.interceptions }
+    when "rushes"
+      players = players.sort { |a,b| b.rushes <=> a.rushes }
+    when "rushing_yards"
+      players = players.sort { |a,b| b.rushing_yards <=> a.rushing_yards }
+    when "rushing_touchdowns"
+      players = players.sort { |a,b| b.rushing_touchdowns <=> a.rushing_touchdowns }
+    when "receptions"
+      players = players.sort { |a,b| b.receptions <=> a.receptions }
+    when "receiving_yards"
+      players = players.sort { |a,b| b.receiving_yards <=> a.receiving_yards }
+    when "receiving_touchdowns"
+      players = players.sort { |a,b| b.receiving_touchdowns <=> a.receiving_touchdowns }
+    end
+
+    players
   end
 end
